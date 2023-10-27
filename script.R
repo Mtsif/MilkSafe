c71ad7b70149e5e0400b6f6b51b34d42b7a4d886
Tsifintaris Margaritis <101420482+Mtsif@users.noreply.github.com>
2023-10-27 18:39:00 +0300

    Update script.R

--- script.R
+++ script.R
@@ -1,24 +1,20 @@
 suppressMessages(library(dplyr))
 suppressMessages(library(UpSetR))
-suppressMessages(library("ggplot2"))
+suppressMessages(library(ggplot2))
 
 args <- commandArgs(trailingOnly = TRUE)
 
 var1 <- args[1]
 var2 <- args[2]
 
-
 df1 = read.table(var1, header = TRUE)
-#df1 = read.table("Proteomics_milk.tsv", header = TRUE)
 df2 = read.table(var2, header = FALSE)
-#df2 = read.table("test_data.tsv", header = FALSE)
-
 
 df2 = df2 %>%
-  mutate(sample = 1)
+	mutate(sample = 1)
 
 result_df = df1 %>%
-  left_join(df2 %>% select(V1, sample), by = c("genes" = "V1"))
+	left_join(df2 %>% select(V1, sample), by = c("genes" = "V1"))
 
 result_df = result_df %>% replace(is.na(.), 0)
 data  = result_df %>% select(-genes)
@@ -28,9 +24,13 @@ exosomes_set = c("human", "bovine", "goat", "donkey", "sample")
 milk_set = c("human_milk", "bovine_milk", "goat_milk", "sample")
 
 # Create two data frames for each panel
-exosomes_data <- data[, exosomes_set]
-milk_data <- data[, milk_set]
-
+exosomes_data = data[, exosomes_set]
+exosomes_data = exosomes_data %>%
+	filter_at(vars(1:5), any_vars(. != 0))
+	
+milk_data = data[, milk_set]
+milk_data = milk_data %>%
+	filter_at(vars(1:4), any_vars(. != 0))
 
 cat("Creating an UpSetPlot for exosome proteins")
 # Create the UpSet plot
@@ -39,17 +39,15 @@ upset(exosomes_data, sets = exosomes_set, order.by = "freq",
 #		set_order = c("a", "b", "c"),
 		mainbar.y.label = "Intersection Size", 
 		sets.x.label = "Set Size",
-#     nsets = 6,
-#		c(intersection size title, intersection size tick labels, set size title, set size tick labels, set names, numbers above bars)
-      text.scale = c(2, 1.5, 2, 1.5, 2, 2),
-      point.size = 4.0, 
-      line.size = 1.7,
-#     sets.bar.color = "lightblue", 
-#		main.bar.color = "lightgray",
+#		nsets = 6,
+#c(intersection size title, intersection size tick labels, set size title, set size tick labels, set names, numbers above bars)
+		text.scale = c(2, 1.5, 2, 1.5, 2, 2),
+		point.size = 4.0, 
+		line.size = 1.7,
 		empty.intersections = "on",
 		keep.order = TRUE,
 #		group.by = "sets"
-      ) 
+) 
 none = dev.off()
 cat(" - Done \n")
 
@@ -59,18 +57,15 @@ jpeg("UpSetPlot_milk.jpg", width = 16, height = 10, units = 'in', res = 600)
 upset(milk_data, sets = milk_set, order.by = "freq",
 		mainbar.y.label = "Intersection Size", 
 		sets.x.label = "Set Size",
-#     nsets = 6,
-#		c(intersection size title, intersection size tick labels, set size title, set size tick labels, set names, numbers above bars)
-      text.scale = c(2, 1.5, 2, 1.5, 2, 2),
-      point.size = 4.0, 
-      line.size = 1.7,
-#     sets.bar.color = "lightblue", 
-#		main.bar.color = "lightgray",
+#		nsets = 6,
+#	c(intersection size title, intersection size tick labels, set size title, set size tick labels, set names, numbers above bars)
+		text.scale = c(2, 1.5, 2, 1.5, 2, 2),
+		point.size = 4.0, 
+		line.size = 1.7,
 		empty.intersections = "on",
 		keep.order = TRUE,
 #		group.by = "sets"
-      )
+)
 none = dev.off()
 cat(" - Done \n")
-
 
